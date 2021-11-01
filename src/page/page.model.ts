6ad80b0716236f113717ba1690e24844a9a5fa9d
Decode Marines <goipukraine@gmail.com>
2021-11-01 10:29:39 +0200

    db models added

--- src/page/page.model.ts
+++ src/page/page.model.ts
@@ -1,26 +1,64 @@
+import { prop } from '@typegoose/typegoose';
+import { TimeStamps, Base } from '@typegoose/typegoose/lib/defaultClasses';
+
 export enum TopLevelCategory {
     Courses,
     Services,
     Books,
     Products,
 }
-export class PageModel {
-    _id: string;
-    topCategory: TopLevelCategory;
-    secondCategory: string;
+
+export class Openings {
+    @prop()
+    count: number;
+
+    @prop()
+    juniorSalary: number;
+
+    @prop()
+    middleSalary: number;
+
+    @prop()
+    seniorSalary: number;
+}
+
+export class TopPageAdvantage {
+    @prop()
     title: string;
+
+    @prop()
+    description: string;
+}
+
+export interface PageModel extends Base {}
+export class PageModel extends TimeStamps {
+    @prop({ enum: TopLevelCategory })
+    topCategory: TopLevelCategory;
+
+    @prop()
+    secondCategory: string;
+
+    @prop({ unique: true })
+    alias: string;
+
+    @prop()
+    title: string;
+
+    @prop()
     category: string;
-    openings?: {
-        count: number;
-        junSalary: number;
-        midSalary: number;
-        senSalary: number;
-    };
-    advantages: {
-        title: string;
-        description: string;
-    }[];
+
+    @prop({ type: () => Openings })
+    openings?: Openings;
+
+    @prop({ type: () => [TopPageAdvantage] })
+    advantages: TopPageAdvantage[];
+
+    @prop()
     seo: string;
+
+    @prop()
     tagsTitle: string;
+
+    @prop(() => [String])
     tags: string[];
 }
--- src/product/product.model.ts
+++ src/product/product.model.ts
@@ -1,17 +1,49 @@
-export class ProductModel {
-    _id: string;
+import { prop } from '@typegoose/typegoose';
+import { TimeStamps, Base } from '@typegoose/typegoose/lib/defaultClasses';
+
+class ProductFeature {
+    @prop()
+    name: string;
+
+    @prop()
+    value: string;
+}
+
+export interface ProductModel extends Base {}
+export class ProductModel extends TimeStamps {
+    @prop()
     image: string;
+
+    @prop()
     title: string;
+
+    @prop()
     price: number;
+
+    @prop()
     oldPrice: number;
+
+    @prop()
     rates: number;
+
+    @prop()
     credit: number;
+
+    @prop()
     description: string;
+
+    @prop()
     advantages: string;
+
+    @prop()
     disadvantages: string;
+
+    @prop({ type: () => [String] })
     categories: string[];
-    tags: string;
-    features: {
-        [key: string]: string;
-    };
+
+    @prop({ type: () => [String] })
+    tags: string[];
+
+    @prop({ type: () => [ProductFeature], _id: false })
+    features: ProductFeature;
 }
