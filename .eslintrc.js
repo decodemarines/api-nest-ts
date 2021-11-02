ee68e40b8837b8cad7c5c06d22e58338523c226a
Decode Marines <goipukraine@gmail.com>
2021-11-02 12:59:04 +0200

    jwt added. Tested with Postman

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,5 +19,6 @@ module.exports = {
         '@typescript-eslint/no-unused-vars': 'off',
         '@typescript-eslint/no-use-before-define': 'off',
         '@typescript-eslint/no-empty-interface': 'off',
+        '@typescript-eslint/camelcase': 'off',
     },
 };
--- src/auth/auth.constants.ts
+++ src/auth/auth.constants.ts
@@ -1,1 +1,3 @@
 export const ALREADY_REGISTERED_ERROR = 'User has already been registered';
+export const USER_NOT_FOUND_ERROR = 'User with such an email was not found';
+export const WRONG_PASSWORD_ERROR = 'Paswword is incorrect';
